db51ddc48d31b6f2d29128f5be5be545c8208a3c
yungtofuu <37648113+yungtofuu@users.noreply.github.com>
2018-05-26 11:03:38 -0400

    Update and rename 13 to weather_script2.py

---
+++ weather_script2.py
@@ -0,0 +1,22 @@
+
+sense = SenseHat()
+logger = Streamer(bucket_name="Sense Hat Sensor Data", access_key="q37KUCDGB9jjsDjxrXiitYz98dqOGqDg")
+sense.clear()
+
+try:
+      while True:
+           temp = sense.get_temperature()
+           temp = 1.8 * round(temp, 1)+ 32
+           logger.log("Temperature F",temp)
+
+           humidity = sense.get_humidity()
+           humidity = round(humidity, 1)
+           logger.log("Humidity :",humidity)
+
+           pressure = sense.get_pressure()
+           pressure = round(pressure, 1)
+           logger.log("Pressure:",pressure)
+
+           time.sleep(1)
+except KeyboardInterrupt:
+      pass
